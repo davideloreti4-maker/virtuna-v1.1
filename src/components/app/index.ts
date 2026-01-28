052c003f5794af97e4870a5894e74e38e2a44560
Davide Loreti <davide.loreti4@gmail.com>
2026-01-28 19:15:29 +0100

    chore(05-03): export CreateSocietyModal from barrel
    - Add CreateSocietyModal to components/app barrel exports

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -8,3 +8,4 @@ export { Sidebar } from "./sidebar";
 export { SidebarNavItem } from "./sidebar-nav-item";
 export { SocietySelector } from "./society-selector";
 export { ViewSelector } from "./view-selector";
+export { CreateSocietyModal } from "./create-society-modal";
