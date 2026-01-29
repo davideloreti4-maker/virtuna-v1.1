7b4a95c111245b9675bc57fce4448c95ea9ca8a3
Davide Loreti <davide.loreti4@gmail.com>
2026-01-29 15:07:23 +0100

    chore(09-02): add barrel exports for settings components
    - Export SettingsPage, ProfileSection, AccountSection

---
+++ src/components/app/settings/index.ts
@@ -0,0 +1,4 @@
+export { SettingsPage } from "./settings-page";
+export { ProfileSection } from "./profile-section";
+export { AccountSection } from "./account-section";
+export { NotificationsSection } from "./notifications-section";
