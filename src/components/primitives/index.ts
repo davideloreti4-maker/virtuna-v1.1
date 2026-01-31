ad489e6ead1f626de1f4d81fe21ca3a8f4858fa5
Davide Loreti <davide.loreti4@gmail.com>
2026-01-31 11:46:48 +0100

    feat(15-02): add barrel export for primitives
    - Export GlassPanel and GlassPanelProps
    - Export GradientGlow, GradientGlowProps, and GradientColor type
    - Components importable from @/components/primitives

---
+++ src/components/primitives/index.ts
@@ -0,0 +1,5 @@
+export { GlassPanel } from "./GlassPanel";
+export type { GlassPanelProps } from "./GlassPanel";
+
+export { GradientGlow } from "./GradientGlow";
+export type { GradientGlowProps, GradientColor } from "./GradientGlow";
