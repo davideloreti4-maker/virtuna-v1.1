5bb0006ffad5a290a7c6834b5d8badaab665fcad
Davide Loreti <davide.loreti4@gmail.com>
2026-02-03 19:27:13 +0100

    feat(40-03): export InputField and InputFieldProps from UI index
    - Add InputField to component exports
    - Add InputFieldProps to type exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,8 +1,8 @@
 export { Button, buttonVariants } from "./button";
 export type { ButtonProps } from "./button";
 
-export { Input } from "./input";
-export type { InputProps } from "./input";
+export { Input, InputField } from "./input";
+export type { InputProps, InputFieldProps } from "./input";
 
 export {
   Card,
