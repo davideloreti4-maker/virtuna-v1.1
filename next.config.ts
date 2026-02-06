8e34d87c6195d410e63139760b2e210937b01641
Davide Loreti <davide.loreti4@gmail.com>
2026-02-06 08:37:04 +0100

    chore(51-01): add picsum.photos remotePatterns + install react-intersection-observer
    - Add picsum.photos and fastly.picsum.photos to images.remotePatterns in next.config.ts
    - Install react-intersection-observer for infinite scroll sentinel
    - Both hostnames needed because picsum.photos redirects to fastly CDN

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['three'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fastly.picsum.photos',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
